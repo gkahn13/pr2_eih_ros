809d4cdb7c4db975e5bc541c3f90f1307c9d360c
Peter Sujan <peterasujan@gmail.com>
2014-09-16 16:54:16 -0700

    Added filegroup functionality to processor.

--- experiment_logger/data/logfile_processor.py
+++ experiment_logger/data/logfile_processor.py
@@ -5,8 +5,100 @@ import numpy as np
 import sys
 import re
 from datetime import datetime, timedelta
+import os
 
 CONFIG = {'verbose': False}
+
+class NumericData:
+    def __init__(self, data = None):
+        if not data: data = []
+        self.data = data
+
+    def add(self, d):
+        self.data.append(d)
+
+    def mean(self):
+        if len(self.data) > 0:
+            return sum(self.data)/float(len(self.data))
+        return None
+
+    def sd(self):
+        m = self.mean
+        if m is None:
+            return None
+        return np.std(np.array(self.data))
+
+class FileGroup:
+    def __init__(self, filenames, name):
+        self.files = []
+        self.runs = []
+        self.name = name
+        self.grasps_attempted = 0
+        self.successful_grasps = 0
+        self.experiment_times = NumericData()
+        self.run_times = NumericData()
+        self.total_time_runs = timedelta(0)
+        self.total_time_experiments = timedelta(0)
+
+        i = 0
+        for filename in filenames:
+            current_file = File("{0}, experiment {1}".format(name, i), filename)
+            i += 1
+            self.files.append(current_file)
+            for run in current_file.split_runs():
+                self.runs.append(run)
+
+       
+        self.calculate_group_statistics()
+        self.calculate_experiment_level_statistics()
+        
+    
+    def calculate_group_statistics(self):
+        for run in self.runs:
+            proc = LogfileProcessor(run)
+            self.grasps_attempted += proc.grasps_attempted
+            self.successful_grasps += proc.successful_grasps
+            self.total_time_runs += proc.total_time()
+            self.run_times.add(proc.total_time().total_seconds())
+
+    def calculate_experiment_level_statistics(self):
+        for f in self.files:
+            proc = LogfileProcessor(f)
+            self.total_time_experiments += proc.total_time()
+            self.experiment_times.add(proc.total_time().total_seconds())
+
+    def average_experiment_time(self):
+        #return timedelta(seconds=self.total_time_experiments.total_seconds() / len(self.files))
+        return timedelta(seconds=self.experiment_times.mean())
+
+    def average_run_time(self):
+        #return timedelta(seconds=self.total_time_runs.total_seconds() / len(self.runs))
+        return timedelta(seconds=self.run_times.mean())
+
+    def sd_experiment_time(self):
+        return timedelta(seconds=self.experiment_times.sd())
+
+    def sd_run_time(self):
+        return timedelta(seconds=self.run_times.sd())
+
+    def grasp_percentage(self):
+        return 100.0 * self.successful_grasps / self.grasps_attempted
+            
+    def summary(self):
+        result = "Summary for group: {0}\n".format(self.name)
+        if self.grasps_attempted:
+           result += "Successful grasps: {0}/{1} ({2:.3g}%)\n".format(self.successful_grasps, self.grasps_attempted, self.grasp_percentage())
+        else:
+            result += "No grasps attempted.\n"
+
+        result += "Ran {0} experiments, for a total of {1} runs.\n".format(len(self.files), len(self.runs))
+        result += "Average experiment time: {0}\n".format(self.average_experiment_time())
+        result += "Standard deviation: {0}\n".format(self.sd_experiment_time())
+        result += "Average run time: {0}\n".format(self.average_run_time())
+        result += "Standard deviation: {0}\n".format(self.sd_run_time())
+            
+        return result
+        
 
 class File:
     def __init__(self, name, file_name=None):
@@ -53,8 +145,10 @@ class LogfileProcessor:
             self.info = defaultdict(list)
             self.name = infile.name
             self.first_handle_time = None
-            self.last_zero_weights = None
-            self.last_nonzero_weights = None
+            self.zero_weights = []
+            self.nonzero_weights = []
+            self.successful_grasps = 0
+            self.grasps_attempted = 0
         
             if CONFIG['verbose']:
                 print "processing {}".format(self.name)
@@ -67,6 +161,7 @@ class LogfileProcessor:
 
             self.total_times = dict()
             self.post_process()
+            
     
     @staticmethod
     def split_line(line):
@@ -112,16 +207,21 @@ class LogfileProcessor:
         if result:
             if line.count('start') > 0:
                 self.start[result['name']].append(result['time'])
+                if line.count("execute_grasp_trajectory") > 0:
+                    self.grasps_attempted += 1
             elif line.count('end') > 0:
                 self.end[result['name']].append(result['time'])
             elif line.count('handles') and self.get_handles(line)['count'] > 0 and not self.first_handle_time:
                 self.first_handle_time = self.get_handles(line)['time']
-            elif line.count('weights'):
+            elif line.count('weights') > 0:
                 count = self.count_weights(line)
                 if count['zero']:
-                    self.last_zero_weights = count['count']
+                    self.zero_weights.append(count['count'])
                 else:
-                    self.last_nonzero_weights = count['count']
+                    self.nonzero_weights.append(count['count'])
+            elif line.count("successful grasp") > 0:
+                self.successful_grasps += 1
+
             
         
             
@@ -136,21 +236,37 @@ class LogfileProcessor:
                     print "[Assuming {0} lasted until end of experiment]".format(name)
                 end_list.append(self.end_of_experiment)
             self.total_times[name] = np.sum(np.array(end_list) - np.array(start_list))
+
+    
+    def total_time(self):
+        return sum(self.total_times.values(), timedelta(0))
+
+    def experiment_duration(self):
+        return self.end_of_experiment - self.start_of_experiment
+        
+    def time_to_first_handle(self):
+        return self.first_handle_time - self.start_of_experiment
+
             
     def summary(self):
         result = "Summary of {0}:\n".format(self.name)
-        timesum = sum(self.total_times.values(), timedelta(0))
+        timesum = self.total_time()
         for name, time in self.total_times.items():
             result += "{0}: {1} ({2:.3g}%)\n".format(name, time, 100 * time.total_seconds() / timesum.total_seconds())
     
         result += "total: {0}\n".format(timesum)
         if self.start_of_experiment:
-            result += "experiment total time: {0}\n".format(self.end_of_experiment - self.start_of_experiment)
+            result += "experiment total time: {0}\n".format(self.experiment_duration())
 
-        result += "\nTime before first handle found: {0}".format(self.first_handle_time - self.start_of_experiment)
-        total_voxels = float(self.last_nonzero_weights + self.last_zero_weights)
-        result += "\nCount of nonzero weight voxels: {0} ({1:.3g}%)\n".format(self.last_nonzero_weights, 100 * self.last_nonzero_weights / total_voxels)
-        result += "Count of zero weight voxels: {0} ({1:.3g}%)\n".format(self.last_zero_weights, 100 * self.last_zero_weights / total_voxels)
+        result += "\nTime before first handle found: {0}".format(self.time_to_first_handle())
+        for i in range(len(self.nonzero_weights)):
+            total_voxels = float(self.nonzero_weights[i] + self.zero_weights[i])
+            result += "\nCount of nonzero weight voxels: {0} ({1:.3g}%)\n".format(self.nonzero_weights[i], 100 * self.nonzero_weights[i] / total_voxels)
+            result += "Count of zero weight voxels: {0} ({1:.3g}%)\n".format(self.zero_weights[i], 100 * self.zero_weights[i] / total_voxels)
+            if self.grasps_attempted > 0:
+                result += "Successful grasps: {0}/{1} ({2}%)\n".format(self.successful_grasps, self.grasps_attempted, 100.0 * self.successful_grasps / self.grasps_attempted)
+            else:
+                result += "No grasps attempted.\n"
         return result
 
 
@@ -161,21 +277,27 @@ if __name__ == '__main__':
         print "Error: no file specified"
         sys.exit(1)
 
-    file_name = sys.argv[1]
     CONFIG["verbose"] = sys.argv.count("-v") > 0
     CONFIG["total"] = sys.argv.count("-t") > 0
 
-    infile = File("all runs", file_name)
-    proc = LogfileProcessor(infile)
-    runs = infile.split_runs()
-    run_procs = []
-    for run in runs[1:]:
-        run_procs.append(LogfileProcessor(run))
+    if sys.argv.count("-g") > 0:
+        groupname = sys.argv[1]
+        group = FileGroup([groupname + "/" + f for f in os.listdir(groupname) if f.count(".txt")], groupname)
+        print group.summary()
+    else:
+        file_name = sys.argv[1]
 
-    print
-    print proc.summary()
-    if not CONFIG["total"]:
-        for run_proc in run_procs:
+        infile = File("all runs", file_name)
+        proc = LogfileProcessor(infile)
+        runs = infile.split_runs()
+        run_procs = []
+        for run in runs[1:]:
+            run_procs.append(LogfileProcessor(run))
+
+        print
+        print proc.summary()
+        if not CONFIG["total"]:
+            for run_proc in run_procs:
+                print
+                print run_proc.summary()
             print
-            print run_proc.summary()
-    print
