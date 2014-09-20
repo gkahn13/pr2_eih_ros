2b832660354514435ecb9a587c4e0ceb9497035e
Greg Kahn <gkahn13@gmail.com>
2014-09-20 07:59:57 -0700

    finished 5-10 of sampling 100 bathroom

--- handle_detector/scripts/handle_poses_publisher.py
+++ handle_detector/scripts/handle_poses_publisher.py
@@ -31,8 +31,8 @@ class HandlePosesPublisher:
         self.handles_thread.start()
         
         # for box
-        self.min_positions = np.array([0.26,-0.9,0.68])
-        self.max_positions = np.array([0.8,0.2,0.86])
+        self.min_positions = np.array([0.28,-0.9,0.7])
+        self.max_positions = np.array([0.7,0.2,0.86])
         
         # for kitchen
 #         self.min_positions = np.array([0.48,-0.9,0.68])
